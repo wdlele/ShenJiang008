f1a20a21d6bdee201206187629d051106dad9f64
wdlele <9815196@qq.com>
2022-03-01 14:40:58 +0800

    V01.02

--- src/COM/Wdlele/Sj001/Test1.java
+++ src/COM/Wdlele/Sj001/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
 
     }
 }
